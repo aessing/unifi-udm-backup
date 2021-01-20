4ac38ed1488f151504f66d45e5c2b97dc31c859a
thibaut severac <thib3113@gmail.com>
2021-01-20 23:12:19 +0100

    Allow to skip "protect" backup (#10)
    * move more informations to variables
    * forgot to update from local changes
    * add comment + move CRON_SCHEDULE

--- on_boot.d/80-udm-backup-ftp.sh
+++ on_boot.d/80-udm-backup-ftp.sh
@@ -21,18 +21,37 @@ echo "------------------------------------------------------------"
 echo " Configure scheduled copy of backups to external FTP server"
 echo "------------------------------------------------------------"
 
-CRON_FILE='/etc/cron.d/udm-backup-ftp'
 
 FTP_SERVER={SERVERNAME}
 FTP_PATH={BACKUPPATH}
 FTP_USER={FTPUSER}
 FTP_PASSWORD={FTPPASSWORD}
 
+CRON_FILE='/etc/cron.d/udm-backup-ftp'
+CRON_SCHEDULE='30 * * * *'
+
+
 SDN_MOUNT="/mnt/data/unifi-os/unifi/data/backup/autobackup:/backups/unifi:ro"
+# you can comment next line to disable protect backup (or if protect is disabled on your UDM)
 PROTECT_MOUNT="/mnt/data_ext/unifi-os/unifi-protect/backups:/backups/protect:ro"
 
+CRON_CMD="${CRON_SCHEDULE} podman run -it --rm --name UDM-FTP-Backup --network=host -e \"FTP_SERVER=$FTP_SERVER\" -e \"FTP_PATH=$FTP_PATH\" -e \"FTP_USER=$FTP_USER\" -e \"FTP_PASSWORD=$FTP_PASSWORD\""
+BACKUP_IMG='docker.io/aessing/udm-backup-ftp'
+
+
+
+if [ ! -z "${SDN_MOUNT}" ]; then
+    CRON_CMD="${CRON_CMD} -v \"$SDN_MOUNT\""
+fi
+
+if [ ! -z "${PROTECT_MOUNT}" ]; then
+    CRON_CMD="${CRON_CMD} -v \"$PROTECT_MOUNT\""
+fi
+
+CRON_CMD="${CRON_CMD} ${BACKUP_IMG}"
+
 if [ ! -f "${CRON_FILE}" ]; then
-    echo "30 * * * * podman run -it --rm --name UDM-FTP-Backup --network=host -e \"FTP_SERVER=$FTP_SERVER\" -e \"FTP_PATH=$FTP_PATH\" -e \"FTP_USER=$FTP_USER\" -e \"FTP_PASSWORD=$FTP_PASSWORD\" -v \"$SDN_MOUNT\" -v \"$PROTECT_MOUNT\" docker.io/aessing/udm-backup-ftp" > ${CRON_FILE}
+    echo "${CRON_CMD}" > ${CRON_FILE}
     chmod 644 ${CRON_FILE}
     /etc/init.d/crond reload ${CRON_FILE}
 fi
